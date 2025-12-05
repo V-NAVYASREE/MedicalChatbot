00e17e093d6466c40acba452a8cd78216af0dd2b
email <navyasree.v2022@vitstudent.ac.in>
2025-12-05 20:01:00 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="medical_chatbot"
+
+    ,version="0.1.0",
+    author="Navya Sree",
+    author_email="navyasreev.work@gmail.com",
+    packages=find_packages(),
+    install_requires=[])
